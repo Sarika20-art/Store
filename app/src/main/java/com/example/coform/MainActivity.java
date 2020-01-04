4fd2f4281c989e7ce323655784e0a5719f22d7a3
Sarika20 <nikitashete06@gmail.com>
2020-01-04 20:26:12 +0530

    asdfghjkl;

--- app/src/main/java/com/example/coform/MainActivity.java
+++ app/src/main/java/com/example/coform/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -18,9 +19,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Savebtn;
-    private EditText editText;
-    private FirebaseFirestore mFirestore;
+
+    private EditText editTextName;
+    private FirebaseFirestore db;
 
 
     @Override
@@ -28,32 +29,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirestore=FirebaseFirestore.getInstance();
-        Savebtn=(Button) findViewById(R.id.button1);
-        editText=(EditText) findViewById(R.id.edittext1);
-
-
-        Savebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username=editText.getText().toString();
-                Map<String,String> usermap=new HashMap<>();
-                usermap.put("name",username);
-
-                mFirestore.collection("aaaa").add(usermap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(MainActivity.this, "Added to firebase", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Something wenr wrong", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-            }
-        });
-
+        db = FirebaseFirestore.getInstance();
+        editTextName = (EditText) findViewById(R.id.edittext1);
+        findViewById(R.id.button1).setOnClickListener((View.OnClickListener) this);
     }
+    private boolean validateInputs(String name){
+        if (name.isEmpty())
+        {
+            editTextName.setError("Name Required");
+            editTextName.requestFocus();
+            return true;
+        }
+        return false;
+    }
+    public void onClick(View view)
+    {
+        String name=editTextName.getText().toString().trim();
+        if (!validateInputs(name))
+        {
+            CollectionReference dbproduct=db.collection("products");
+
+            Product product=new Product(name);
+
+            dbproduct.add(product).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Not Success", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
 }
+
